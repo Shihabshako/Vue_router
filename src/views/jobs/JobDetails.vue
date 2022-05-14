45b49f399eacc7e91dd1b8d8f060404eabbd6da1
Shihab Jamil <sjamil181180@bscse.uiu.ac.bd>
2022-05-14 23:23:42 +0600

    bug fix route param

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -9,7 +9,7 @@
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 export default {
-    // props : ['id'],
+    props : ['id'],
     setup(props){
         // const route = useRoute()
         // const id = ref(route.params.id)
